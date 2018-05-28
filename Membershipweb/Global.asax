c9aafcbfc85418e49427e6968a6562f1cb254d3e
Owner <Owner@MAHINTHAN>
2018-05-28 17:21:56 +0530

    Add project files.

---
+++ Membershipweb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Membershipweb.MvcApplication" Language="C#" %>
---
+++ Membershipweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Membershipweb.Startup))]
+namespace Membershipweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
